5e243e2dd34630dd9c076d14a591073af30a8349
vyhuynh147 <huynhyenvy140709@gmail.com>
2025-10-15 22:47:33 -0400

    food

--- main.py
+++ main.py
@@ -1,37 +1,47 @@
 import os
+import random 
 os.system('cls||clear')
 
-import random 
 
-play_again = True
+secret_food = ["sushi", "noodle", "salad", "steak", "pasta", "chicken", "nuggets", "pancake"]
+#play_again = True
 
-
-print("Welcome to the Food Guessing Game!\n")
-
-secret_food = ["Sushi", "noodle", "salad", "steak", "pasta", "chicken", "nuggets", "pancake"]
-print(secret_food)
-random.choice(secret_food)
 
 while True: 
-    print("Try to guess the food that I'm thinking of...")
+    # chon mon an ngau nhien
+    food = random.choice(secret_food) 
 
+    print("Welcome to the Food Guessing Game!\n")
+    print("Try to guess the food that I'm thinking of...")
+    print("Here is the foods you can guess:", secret_food)
+# 
     max_attempts = 3
     attempt = 0
+    # check if user has used hint
+    use_hint = False 
+
     while attempt < max_attempts:
         guess = input("Enter your guess: ").lower().strip()
         attempt +=1
-    if guess == secret_food:
-        print(" Congratulation, You guessed right", attempt, "tries")
-    else: 
-        hint = secret_food[0]
-        print(" Wrong, Try again!!! Hint: The letter food starts with",hint)
-        if guess != secret_food: 
-            print(" Sorry, The food was",secret_food)   
+        if guess == food:
+            print(" Congratulations, You guessed right", attempt, "tries")
+            break
+        else: 
+            print(" Wrong, Try again!!!")
+        # if 
+            if not use_hint:
+                hint = input(" Do you want to use hint? (yes or no):")
+                if hint == "yes":
+                    print(" Hint: The food starts with",food[0])
+                    use_hint = True
 
-            play_again = input(" Do you want to play again? (yes or no)").lower().strip()
-            if play_again != "yes":
-                break
-            print(" Thank you for playing Guess the Food!")
+    if guess != food: 
+        print(" Sorry, The food was",food)   
+
+    play_again = input("Do you want to play again? (yes/no): ").lower().strip()
+    if play_again != "yes":
+        print("Thank you for playing Guess the Food!")
+        break
 
 
 
