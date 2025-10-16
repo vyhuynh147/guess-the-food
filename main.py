05882128b64f7664066eee3ee533bbbd515506d7
vyhuynh147 <huynhyenvy140709@gmail.com>
2025-10-15 21:55:15 -0400

    not done

--- main.py
+++ main.py
@@ -3,17 +3,36 @@ os.system('cls||clear')
 
 import random 
 
+play_again = True
+
+
 print("Welcome to the Food Guessing Game!\n")
 
 secret_food = ["Sushi", "noodle", "salad", "steak", "pasta", "chicken", "nuggets", "pancake"]
 print(secret_food)
-
 random.choice(secret_food)
-play_again = True
+
 while True: 
     print("Try to guess the food that I'm thinking of...")
-    input(" Guess food: ")
-    
+
+    max_attempts = 3
+    attempt = 0
+    while attempt < max_attempts:
+        guess = input("Enter your guess: ").lower().strip()
+        attempt +=1
+    if guess == secret_food:
+        print(" Congratulation, You guessed right", attempt, "tries")
+    else: 
+        hint = secret_food[0]
+        print(" Wrong, Try again!!! Hint: The letter food starts with",hint)
+        if guess != secret_food: 
+            print(" Sorry, The food was",secret_food)   
+
+            play_again = input(" Do you want to play again? (yes or no)").lower().strip()
+            if play_again != "yes":
+                break
+            print(" Thank you for playing Guess the Food!")
+
 
 
 
